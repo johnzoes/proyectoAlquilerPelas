7d564af45a23f8ad1c7d9602b7b4d72fad8e357b
johncito <johnzorrillx@gmail>
2023-11-08 21:09:15 -0500

    olaaa

--- LoginPanel.java
+++ LoginPanel.java
@@ -8,31 +8,19 @@ public class LoginPanel extends JPanel{
     JTextField txtusuario, txtcontraseña;
     JLabel lbl;
     public LoginPanel(){
-        setLayout(new GridBagLayout());
-        GridBagConstraints constraints = new GridBagConstraints();
+        setLayout(null);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int ancho = (int) screenSize.getWidth();
+        int alto = (int) screenSize.getHeight();
 
-        txtusuario = new JTextField(20);
+        txtusuario = new JTextField();
+        txtusuario.setBounds(ancho/2-150, alto/2 -70,200,40);
+        add(txtusuario);
+        txtcontraseña = new JTextField();
+        txtcontraseña.setBounds(ancho/2-150, alto/2 -150,200,40);
+        add(txtcontraseña);
 
-        txtcontraseña = new JPasswordField(20);
 
-    JButton loginButton = new JButton("Iniciar Sesión");
-
-    constraints.insets = new Insets(5, 0, 5,5 );
-
-        constraints.gridx = 1;
-        constraints.gridy = 0;
-        constraints.gridheight = 2;
-        add(txtusuario, constraints);
-
-        constraints.gridx = 1;
-        constraints.gridy = 1;
-        add(txtcontraseña, constraints);
-
-        constraints.gridx = 0;
-        constraints.gridy = 2;
-        constraints.gridwidth = 2;
-        constraints.anchor = GridBagConstraints.CENTER;
-        add(loginButton, constraints);
 
     }
 }

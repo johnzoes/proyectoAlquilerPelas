72b7a7c06e041f182934b9ec028de66244418280
jeremy <jermybailonpua@gmail.com>
2023-11-08 20:23:07 -0500

    cambio

--- LoginPanel.java
+++ LoginPanel.java
@@ -7,6 +7,7 @@ import java.awt.event.ActionListener;
 public class LoginPanel extends JPanel{
     JTextField txtusuario, txtcontraseña;
     JLabel lbl;
+    JButton btnIngresar;
     public LoginPanel(){
         setLayout(new GridLayout(3,2));
         txtusuario = new JTextField(20);
@@ -14,5 +15,10 @@ public class LoginPanel extends JPanel{
 
         txtcontraseña = new JTextField(10);
         add(txtcontraseña);
+
+        btnIngresar=new JButton("Ingresar");
+        btnIngresar.setBounds(50, 50, 50, 30);
+        btnIngresar.addActionListener(this);
+        add(btnIngresar);
     }
 }

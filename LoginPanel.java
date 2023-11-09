0f50e5f63204a5b13b458634bda06920642543db
johncito <johnzorrillx@gmail>
2023-11-08 20:21:19 -0500

    mold

--- LoginPanel.java
+++ LoginPanel.java
@@ -8,11 +8,31 @@ public class LoginPanel extends JPanel{
     JTextField txtusuario, txtcontraseña;
     JLabel lbl;
     public LoginPanel(){
-        setLayout(new GridLayout(3,2));
-        txtusuario = new JTextField(20);
-        add(txtusuario);
+        setLayout(new GridBagLayout());
+        GridBagConstraints constraints = new GridBagConstraints();
 
-        txtcontraseña = new JTextField(10);
-        add(txtcontraseña);
+        txtusuario = new JTextField(20);
+
+        txtcontraseña = new JPasswordField(20);
+
+    JButton loginButton = new JButton("Iniciar Sesión");
+
+    constraints.insets = new Insets(5, 0, 5,5 );
+
+        constraints.gridx = 1;
+        constraints.gridy = 0;
+        constraints.gridheight = 2;
+        add(txtusuario, constraints);
+
+        constraints.gridx = 1;
+        constraints.gridy = 1;
+        add(txtcontraseña, constraints);
+
+        constraints.gridx = 0;
+        constraints.gridy = 2;
+        constraints.gridwidth = 2;
+        constraints.anchor = GridBagConstraints.CENTER;
+        add(loginButton, constraints);
+
     }
 }

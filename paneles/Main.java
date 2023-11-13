20921d0295d9db8839ea3b7dcafd94a3275e02ca
john <johnzorrillx@gmail>
2023-11-12 21:25:28 -0500

    aa

---
+++ paneles/Main.java
@@ -0,0 +1,20 @@
+package paneles;
+import javax.swing.JFrame;
+
+import java.awt.*;
+public class Main{ 
+public static void main(String[] args) {
+
+     JFrame ventana;
+    ventana = new JFrame("Alquiler de pela");
+    ventana.add(new LoginPanel());
+    ventana.setExtendedState(JFrame.MAXIMIZED_BOTH);
+    ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    ventana.setBackground(new Color(0,38,85));
+
+    ventana.setVisible(true);
+    
+     
+    }
+
+}
